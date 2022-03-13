3c4593fb1d97cf4f09cc20c99a02ac5d032fd957
Wajhul Huda <62687753+Wajhulhuda@users.noreply.github.com>
2022-03-13 19:48:20 +0530

    Add files via upload

---
+++ March/Week1/Assignment6/KthSymbolInGrammer.java
@@ -0,0 +1,22 @@
+package recursion_IV_assignment;
+
+public class KthSymbolInGrammer {
+
+	public static int kthGrammar(int n, int k) {
+		if (k == 1 && n == 1)
+			return 0;
+		int mid = (int) Math.pow(2, n - 1) / 2;
+		if (k <= mid)
+			return kthGrammar(n - 1, k);
+		else
+			return 1 ^ (kthGrammar(n - 1, k - mid));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=2;
+		int k=2;
+		System.out.println(kthGrammar(n, k));
+	}
+
+}
